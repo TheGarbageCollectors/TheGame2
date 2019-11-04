dfbc037259e10731e0c3b2b1dbd8fe5fc87dc273
Benji703 <55736985+Benji703@users.noreply.github.com>
2019-11-04 15:09:56 +0100

    Added body

--- src/worldofzuul/GameItems.java
+++ src/worldofzuul/GameItems.java
@@ -9,7 +9,32 @@ package worldofzuul;
  *
  * @author GamerQuvang
  */
-public interface GameItems
+public class GameItems
 {
+    Item plasticBottle = new Item("Plastik Flaske", new String[]{"Plastik"});
+        Item cardBoard = new Item("Papkasse", new String[]{"Pap"});
+        Item battery = new Item("Batteri", new String[]{"Metal"});
+        Item concrete = new Item("Beton", new String[]{"Beton"});
+        
+        if (room.getName()=="Vej")
+        {   
+                items.add(plasticBottle);
+                items.add(cardBoard);
+        } else if (room.getName()=="Strand") {
+            items.add(plasticBottle);
+        } else if (room.getName()=="Skov") {
+            items.add(plasticBottle);
+        } else if (room.getName()=="Forladt by") {
+            items.add(concrete);
+        } else {
+            throw new UnsupportedOperationException("The room with name: " + 
+                    room.getName() + " has not been implemented");
+        }
+    }
+        
+        public ArrayList getLootList(String name)
+        {
+            return this.items;
+        }
     
 }
