b4527ca591bd268bf184ac1737d2baf6e84e33e0
TrickyMaker <sikri19@student.sdu.dk>
2019-11-04 16:32:22 +0100

    Update GameItems.java

--- src/worldofzuul/GameItems.java
+++ src/worldofzuul/GameItems.java
@@ -9,31 +9,39 @@ package worldofzuul;
  *
  * @author GamerQuvang
  */
-public class GameItems
+public final class GameItems
 {
-    Item plasticBottle = new Item("Plastik Flaske", new String[]{"Plastik"});
+    
+        private ArrayList<Item> items = new ArrayList<>();
+        Item plasticBottle = new Item("Plastik Flaske", new String[]{"Plastik"});
         Item cardBoard = new Item("Papkasse", new String[]{"Pap"});
         Item battery = new Item("Batteri", new String[]{"Metal"});
         Item concrete = new Item("Beton", new String[]{"Beton"});
+    
+    private GameItems();
         
-        if (room.getName()=="Vej")
-        {   
-                items.add(plasticBottle);
-                items.add(cardBoard);
-        } else if (room.getName()=="Strand") {
-            items.add(plasticBottle);
-        } else if (room.getName()=="Skov") {
-            items.add(plasticBottle);
-        } else if (room.getName()=="Forladt by") {
-            items.add(concrete);
-        } else {
-            throw new UnsupportedOperationException("The room with name: " + 
-                    room.getName() + " has not been implemented");
+    private void makeLootlists(String name){
+        
+        switch(name){
+                case "Vej":
+                    items.add(plasticBottle
+                    items.add(cardBoard);
+                    break;
+                case "Strand":
+                    items.add(plasticBottle);
+                     break;
+                case "Skov:
+                       items.add(plasticBottle);
+                       break;
+                case "Forladt by
+                       items.add(concrete);
+                       break;
         }
     }
         
         public ArrayList getLootList(String name)
         {
+            makeLootlists(name);
             return this.items;
         }
     
