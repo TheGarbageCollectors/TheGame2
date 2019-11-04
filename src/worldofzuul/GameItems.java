5186e48896b444f41530333074f6be250b5f0751
TrickyMaker <sikri19@student.sdu.dk>
2019-11-04 17:34:48 +0100

    Update GameItems.java

--- src/worldofzuul/GameItems.java
+++ src/worldofzuul/GameItems.java
@@ -9,40 +9,59 @@ package worldofzuul;
  *
  * @author GamerQuvang
  */
-public final class GameItems
-{
-    
-        private ArrayList<Item> items = new ArrayList<>();
-        Item plasticBottle = new Item("Plastik Flaske", new String[]{"Plastik"});
-        Item cardBoard = new Item("Papkasse", new String[]{"Pap"});
-        Item battery = new Item("Batteri", new String[]{"Metal"});
-        Item concrete = new Item("Beton", new String[]{"Beton"});
-    
-    private GameItems();
-        
-    private void makeLootlists(String name){
-        
-        switch(name){
-                case "Vej":
-                    items.add(plasticBottle);
-                    items.add(cardBoard);
-                    break;
-                case "Strand":
-                    items.add(plasticBottle);
-                     break;
-                case "Skov":
-                       items.add(plasticBottle);
-                       break;
+import java.util.ArrayList;
+
+public final class GameItems {
+
+    private ArrayList<Item> items = new ArrayList<>();
+    private Item plasticBottle;
+    private Item doorFrame;
+    private Item cardBoard;
+    private Item battery;
+    private Item concrete;
+    private Item can;
+    private Item laptop;
+
+    private GameItems() {
+    }
+
+    private void makeItems() {
+        plasticBottle = new Item("PlastikFlaske", new String[]{"Plastik", "Beton"});
+        doorFrame = new Item("DoorFrame", new String[]{"Metal", "Beton"});
+        cardBoard = new Item("Papkasse", new String[]{"Pap"});
+        battery = new Item("Batteri", new String[]{"Metal"});
+        concrete = new Item("Beton", new String[]{"Beton"});
+        can = new Item("Can", new String[]{"Metal"});
+        laptop = new Item("Laptop", new String[]{"Metal", "Plastik"});
+    }
+
+    private void makeLootlists(String name) {
+        //Using clear to remove everything from the arraylist if there is anything. 
+        items.clear();
+
+        items.add(can);
+        items.add(laptop);
+        switch (name) {
+            case "Vej":
+                items.add(plasticBottle);
+                items.add(cardBoard);
+                break;
+            case "Strand":
+                items.add(plasticBottle);
+                break;
+            case "Skov":
+                items.add(plasticBottle);
+                break;
             case "Forladt by":
-                       items.add(concrete);
-                       break;
+                items.add(concrete);
+                items.add(doorFrame);
+                break;
         }
     }
-        
-        public ArrayList getLootList(String name)
-        {
-            makeLootlists(name);
-            return this.items;
-        }
-    
+
+    public ArrayList getLootList(String name) {
+        makeLootlists(name);
+        return this.items;
+    }
+
 }
