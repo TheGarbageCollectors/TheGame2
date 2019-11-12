72ebcb18ef23538ee1cffe30f2db81baf4dcd52e
Drandoss <55736370+Drandoss@users.noreply.github.com>
2019-11-12 16:03:11 +0100

    Stuff

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -20,33 +20,34 @@ public class Game
 
     private void createRooms()
     {
-        Room abandonedVillage = new Lootable("an abandoned villiage", "Abandoned Village");
-        Room road = new Lootable("a long road", "Road");
-        Room beach = new Lootable("a beach", "Beach");
-        Room forrest = new Lootable("lots of trees", "Forrest");
+        Room abandonedVillage = new Lootable("in an abandoned villiage", "Abandoned Village");
+        Room road = new Lootable("on a long road", "Road");
+        Room beach = new Lootable("on a beach", "Beach");
+        Room forrest = new Lootable("in a forrest", "Forrest");
         
-        recycler = new Recycler("a recycler", "Recycler");
-        town = new Town("the lovely town", "Town");
+        recycler = new Recycler("at the recycler", "Recycler");
+        town = new Town("by the Town Hall", "Town Hall");
         upgradeStation = new UpgradeStation("an upgradestation", "Upgrade Station");
 
-        town.setExit("road", road);
-        town.setExit("recycler", recycler);
-        town.setExit("upgradestation", upgradeStation);
+        town.setExit("Road", road);
+        town.setExit("Recycler", recycler);
+        town.setExit("Upgradestation", upgradeStation);
 
-        road.setExit("beach", beach);
-        road.setExit("forrest", forrest);
-        road.setExit("abandonedVillage", abandonedVillage);
-        road.setExit("town", town);
+        road.setExit("Beach", beach);
+        road.setExit("Forrest", forrest);
+        road.setExit("Abandoned_Village", abandonedVillage);
+        road.setExit("Town_Hall", town);
 
-        recycler.setExit("town", town);
+        recycler.setExit("Town", town);
 
-        beach.setExit("road", road);
+        beach.setExit("Road", road);
 
-        upgradeStation.setExit("town", town);
+        upgradeStation.setExit("Town Hall", town);
 
-        forrest.setExit("road", road);
+        forrest.setExit("Road", road);
 
-        road.setExit("road", road);
+        abandonedVillage.setExit("Road", road);
+        //road.setExit("road", road);
 
         currentRoom = town;
     }
@@ -67,8 +68,11 @@ public class Game
     private void printWelcome()
     {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Welcome to the Garbage Collectors!");
+        System.out.println("This is a game about collecting different sorts of garbage and learning how to sort it properly.");
+        System.out.println("The goal of the game is to raise your populations happiness-level");
+        System.out.println("The local area is littered with garbage");
+        System.out.println("It's your job as the mayor to make the town happy, by keeping the streets clean");
         System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
@@ -105,6 +109,10 @@ public class Game
         {
             recycleItems(command);
         }
+        else if (commandWord == CommandWord.INVENTORY)
+        {
+            openInventory(command);
+        }
 
         return wantToQuit;
     }
@@ -112,7 +120,7 @@ public class Game
     private void printHelp()
     {
         System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("around the Town.");
         System.out.println();
         System.out.println("Your command words are:");
         parser.showCommands();
@@ -134,7 +142,6 @@ public class Game
                 if (itemToBeRecycled.equals(itemsInBag.get(i).getName()))
                 {
                     var money = ((Recycler) currentRoom).valueCalculator(itemsInBag.get(i));
-                    System.out.println(money);
                     player1.addMoney(money);
                 }
             }
@@ -156,15 +163,18 @@ public class Game
             {
                 case "town":
                     ((UpgradeStation)upgradeStation).buyUpgrade(town, player1);
+                    System.out.println("Town Hall level is now: " + ((Upgradeable)town).getLevel());
                     break;
                 case "recycler":
                     ((UpgradeStation)upgradeStation).buyUpgrade(recycler, player1);
+                    System.out.println("Recycler level is now: " + ((Upgradeable)recycler).getLevel());
                     break;
                 case "backpack":
                     ((UpgradeStation)upgradeStation).buyUpgrade(player1.getBackpackObj(), player1);
+                    System.out.println("Backpack level is now: " + ((Upgradeable)player1.getBackpackObj()).getLevel());
                     break;
             }
-            System.out.println(((Upgradeable)town).getLevel());
+            
         }
 
     }
@@ -200,10 +210,11 @@ public class Game
                         System.out.print(", ");
                     }
                 }
+                System.out.println();
             } 
             else if (nextRoom instanceof Town)
             {
-                System.out.println("You have " + ((Town) nextRoom).getPopulationCount() + " slaves");
+                System.out.println("The Town's happiness level: " + ((Town) nextRoom).getPopulationCount());
             } 
             else if (nextRoom instanceof UpgradeStation)
             {
@@ -211,7 +222,7 @@ public class Game
             } 
             else if (nextRoom instanceof Recycler)
             {
-                System.out.print("You can recycle your stuff here");
+                System.out.println("You can recycle your stuff here");
             }
 
         }
@@ -225,7 +236,7 @@ public class Game
             var items = gameitems.getLootList(((Lootable) currentRoom).getName());
             for (int i = 0; i < items.size(); i++)
             {
-                System.out.print(items.get(i).getName().equals(command.getSecondWord()));
+                //System.out.print(items.get(i).getName().equals(command.getSecondWord()));
                 if (items.get(i).getName().equals(command.getSecondWord()))
                 {
                     player1.getBackpackObj().addItem(items.get(i));
@@ -236,6 +247,18 @@ public class Game
         {
             System.out.println("Pick up what?");
         }
+    }
+    
+    private void openInventory(Command command) {
+        ArrayList<Item> temp = player1.getBackpackObj().getItemsInBackpack();
+        System.out.print("Your inventory contains: ");
+        for (int i = 0; i < temp.size() ; i++) {
+            System.out.print(temp.get(i).getName());
+            if (i+1 != temp.size()) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("");
     }
 
     private boolean quit(Command command)
