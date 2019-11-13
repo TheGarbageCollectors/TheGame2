f95819c50c282e9fcdab930200db3301f87a6331
Drandoss <55736370+Drandoss@users.noreply.github.com>
2019-11-13 21:33:09 +0100

    Fixed some bugs

--- src/worldofzuul/UpgradeStation.java
+++ src/worldofzuul/UpgradeStation.java
@@ -43,9 +43,9 @@ public class UpgradeStation extends Room
                 int recyclerLevel = ((Upgradeable) recycler).getLevel();
                 System.out.println("You are in upgrade station");
                 System.out.println("You can buy upgrades for Backpack, Town and Recycler");
-                System.out.println("Next upgrade for Backpack is: " + (backpackLevel + 1) + ". level that costs: " + backpackUpgradePriceArray[backpackLevel]);
-                System.out.println("Next upgrade for Town_Hall is: " + (townLevel + 1) + ". level that costs: " + townUpgradePriceArray[backpackLevel]);
-                System.out.println("Next upgrade for Recycler is: " + (recyclerLevel + 1)  + ". level that costs: " + recyclerUpgradeArray[backpackLevel]);
+               System.out.println("Next upgrade for Backpack is: " + (backpackLevel + 1) + ". level that costs: " + backpackUpgradePriceArray[backpackLevel]);
+                System.out.println("Next upgrade for Town_Hall is: " + (townLevel + 1) + ". level that costs: " + townUpgradePriceArray[townLevel]);
+                System.out.println("Next upgrade for Recycler is: " + (recyclerLevel + 1)  + ". level that costs: " + recyclerUpgradeArray[recyclerLevel]);
     }
 
     public void buyUpgrade(Object obj, Player playerObj)
@@ -91,7 +91,7 @@ public class UpgradeStation extends Room
                         }
                     }
                     break;
-                case "backpack":
+                case "Backpack":
                     if (((Upgradeable) obj).getLevel() < backpackUpgradePriceArray.length)
                     {
                         upgradePrice = backpackUpgradePriceArray[((Upgradeable) obj).getLevel()];
