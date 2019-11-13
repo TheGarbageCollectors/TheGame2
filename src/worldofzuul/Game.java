e589f686f58b5632d5aa63a7c01512e4f9cb5cfc
Simon Quvang <sikri19@student.sdu.dk>
2019-11-13 11:50:08 +0100

    Fixed cast handling exceptiong when looting a room that wasnt a lootable room

--- src/worldofzuul/Game.java
+++ src/worldofzuul/Game.java
@@ -3,28 +3,25 @@ package worldofzuul;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Game
-{
+public class Game {
 
     private Parser parser;
     private Room currentRoom, town, recycler, upgradeStation;
 
     private Player player1 = new Player();
 
-    public Game()
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
 
     }
 
-    private void createRooms()
-    {
+    private void createRooms() {
         Room abandonedVillage = new Lootable("in an abandoned villiage", "abandoned village");
         Room road = new Lootable("on a long road", "road");
         Room beach = new Lootable("on a beach", "beach");
         Room forrest = new Lootable("in a forrest", "forrest");
-        
+
         recycler = new Recycler("at the recycler", "recycler");
         town = new Town("by the Town Hall", "town hall");
         upgradeStation = new UpgradeStation("an upgradestation", "upgrade station");
@@ -52,21 +49,18 @@ public class Game
         currentRoom = town;
     }
 
-    public void play()
-    {
+    public void play() {
         printWelcome();
 
         boolean finished = false;
-        while (!finished)
-        {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
         System.out.println("Thank you for playing.  Good bye.");
     }
 
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
         System.out.println("Welcome to the Garbage Collectors!");
         System.out.println("This is a game about collecting different sorts of garbage and learning how to sort it properly.");
@@ -78,47 +72,36 @@ public class Game
         System.out.println(currentRoom.getLongDescription());
     }
 
-    private boolean processCommand(Command command)
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
         CommandWord commandWord = command.getCommandWord();
 
-        if (commandWord == CommandWord.UNKNOWN)
-        {
+        if (commandWord == CommandWord.UNKNOWN) {
             System.out.println("I don't know what you mean...");
             return false;
         }
 
-        if (commandWord == CommandWord.HELP)
-        {
+        if (commandWord == CommandWord.HELP) {
             printHelp();
-        } else if (commandWord == CommandWord.GO)
-        {
+        } else if (commandWord == CommandWord.GO) {
             goRoom(command);
-        } else if (commandWord == CommandWord.QUIT)
-        {
+        } else if (commandWord == CommandWord.QUIT) {
             wantToQuit = quit(command);
-        } else if (commandWord == CommandWord.PICKUP)
-        {
+        } else if (commandWord == CommandWord.PICKUP) {
             pickupItems(command);
-        } else if (commandWord == CommandWord.UPGRADE)
-        {
+        } else if (commandWord == CommandWord.UPGRADE) {
             upgradeItems(command);
-        } else if (commandWord == CommandWord.RECYCLE)
-        {
+        } else if (commandWord == CommandWord.RECYCLE) {
             recycleItems(command);
-        }
-        else if (commandWord == CommandWord.INVENTORY)
-        {
+        } else if (commandWord == CommandWord.INVENTORY) {
             openInventory(command);
         }
 
         return wantToQuit;
     }
 
-    private void printHelp()
-    {
+    private void printHelp() {
         System.out.println("You are lost. You are alone. You wander");
         System.out.println("around the Town.");
         System.out.println();
@@ -126,21 +109,17 @@ public class Game
         parser.showCommands();
 
     }
-    private void recycleItems(Command command)
-    {
-        if (!command.hasSecondWord())
-        {
+
+    private void recycleItems(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Recycle what?");
             return;
         }
-        if (currentRoom instanceof Recycler)
-        {
+        if (currentRoom instanceof Recycler) {
             String itemToBeRecycled = command.getSecondWord();
             var itemsInBag = player1.getBackpackObj().getItemsInBackpack();
-            for (int i = 0; i < itemsInBag.size(); i++)
-            {
-                if (itemToBeRecycled.equals(itemsInBag.get(i).getName()))
-                {
+            for (int i = 0; i < itemsInBag.size(); i++) {
+                if (itemToBeRecycled.equals(itemsInBag.get(i).getName())) {
                     var money = ((Recycler) currentRoom).valueCalculator(itemsInBag.get(i));
                     player1.addMoney(money);
                 }
@@ -149,40 +128,34 @@ public class Game
 
     }
 
-    private void upgradeItems(Command command)
-    {
-        if (!command.hasSecondWord())
-        {
+    private void upgradeItems(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Upgrade what?");
             return;
         }
-        if (currentRoom instanceof UpgradeStation)
-        {
+        if (currentRoom instanceof UpgradeStation) {
             String thingToUpgrade = command.getSecondWord();
-            switch (thingToUpgrade)
-            {
+            switch (thingToUpgrade) {
                 case "town":
-                    ((UpgradeStation)upgradeStation).buyUpgrade(town, player1);
-                    System.out.println("Town Hall level is now: " + ((Upgradeable)town).getLevel());
+                    ((UpgradeStation) upgradeStation).buyUpgrade(town, player1);
+                    System.out.println("Town Hall level is now: " + ((Upgradeable) town).getLevel());
                     break;
                 case "recycler":
-                    ((UpgradeStation)upgradeStation).buyUpgrade(recycler, player1);
-                    System.out.println("Recycler level is now: " + ((Upgradeable)recycler).getLevel());
+                    ((UpgradeStation) upgradeStation).buyUpgrade(recycler, player1);
+                    System.out.println("Recycler level is now: " + ((Upgradeable) recycler).getLevel());
                     break;
                 case "backpack":
-                    ((UpgradeStation)upgradeStation).buyUpgrade(player1.getBackpackObj(), player1);
-                    System.out.println("Backpack level is now: " + ((Upgradeable)player1.getBackpackObj()).getLevel());
+                    ((UpgradeStation) upgradeStation).buyUpgrade(player1.getBackpackObj(), player1);
+                    System.out.println("Backpack level is now: " + ((Upgradeable) player1.getBackpackObj()).getLevel());
                     break;
             }
-            
+
         }
 
     }
 
-    private void goRoom(Command command)
-    {
-        if (!command.hasSecondWord())
-        {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             System.out.println("Go where?");
             return;
         }
@@ -191,84 +164,69 @@ public class Game
 
         Room nextRoom = currentRoom.getExit(direction);
 
-        if (nextRoom == null)
-        {
+        if (nextRoom == null) {
             System.out.println("There is no door!");
-        } else
-        {
+        } else {
             currentRoom = nextRoom;
             System.out.println(currentRoom.getLongDescription());
-            if (nextRoom instanceof Lootable)
-            {
+            if (nextRoom instanceof Lootable) {
                 System.out.print("You see ");
                 var items = ((Lootable) nextRoom).getLoot();
-                for (int i = 0; i < items.size(); i++)
-                {
+                for (int i = 0; i < items.size(); i++) {
                     System.out.print(items.get(i).getName());
-                    if ((i + 1) != items.size())
-                    {
+                    if ((i + 1) != items.size()) {
                         System.out.print(", ");
                     }
                 }
                 System.out.println();
-            } 
-            else if (nextRoom instanceof Town)
-            {
+            } else if (nextRoom instanceof Town) {
                 System.out.println("The Town's happiness level: " + ((Town) nextRoom).getPopulationCount());
-            } 
-            else if (nextRoom instanceof UpgradeStation)
-            {
+            } else if (nextRoom instanceof UpgradeStation) {
                 ((UpgradeStation) upgradeStation).welcomeMessage(player1.getBackpackObj(), town, recycler);
-            } 
-            else if (nextRoom instanceof Recycler)
-            {
+            } else if (nextRoom instanceof Recycler) {
                 System.out.println("You can recycle your stuff here");
             }
 
         }
     }
 
-    private void pickupItems(Command command)
-    {
-        if (command.hasSecondWord())
-        {
-            GameItems gameitems = new GameItems();
-            var items = gameitems.getLootList(((Lootable) currentRoom).getName());
-            for (int i = 0; i < items.size(); i++)
-            {
-                //System.out.print(items.get(i).getName().equals(command.getSecondWord()));
-                if (items.get(i).getName().equals(command.getSecondWord()))
-                {
-                    player1.getBackpackObj().addItem(items.get(i));
+    private void pickupItems(Command command) {
+        if (command.hasSecondWord()) {
+            if (currentRoom instanceof Lootable) {
+                GameItems gameitems = new GameItems();
+                var items = gameitems.getLootList(((Lootable) currentRoom).getName());
+                for (int i = 0; i < items.size(); i++) {
+                    //System.out.print(items.get(i).getName().equals(command.getSecondWord()));
+                    if (items.get(i).getName().equals(command.getSecondWord())) {
+                        player1.getBackpackObj().addItem(items.get(i));
+                    }
                 }
+            }else{
+                System.out.println("You are not in a room where you can pick anything up");
             }
 
-        } else
-        {
+        } else {
             System.out.println("Pick up what?");
         }
     }
-    
+
     private void openInventory(Command command) {
         ArrayList<Item> temp = player1.getBackpackObj().getItemsInBackpack();
         System.out.print("Your inventory contains: ");
-        for (int i = 0; i < temp.size() ; i++) {
+        for (int i = 0; i < temp.size(); i++) {
             System.out.print(temp.get(i).getName());
-            if (i+1 != temp.size()) {
+            if (i + 1 != temp.size()) {
                 System.out.print(", ");
             }
         }
         System.out.println("");
     }
 
-    private boolean quit(Command command)
-    {
-        if (command.hasSecondWord())
-        {
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
             System.out.println("Quit what?");
             return false;
-        } else
-        {
+        } else {
             return true;
         }
     }
