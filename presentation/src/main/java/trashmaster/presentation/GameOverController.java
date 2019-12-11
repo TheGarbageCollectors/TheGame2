5c3a476522b0f53a14836e80a57e70776f48662e
Drandoss <daniel.danielcn@gmail.com>
2019-12-11 21:29:40 +0100

    Stuff

--- presentation/src/main/java/trashmaster/presentation/GameOverController.java
+++ presentation/src/main/java/trashmaster/presentation/GameOverController.java
@@ -7,16 +7,34 @@ import javafx.animation.FadeTransition;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
 
 public class GameOverController implements Initializable {
-
+          private UserGUI gui;
     @FXML private StackPane root;
+    @FXML private AnchorPane AnchorPane;
+    @FXML private ImageView recycle1, recycle2, recycle3;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        this.gui = PrimaryController.getGUI();
+        int recyclerLvl = gui.game.getRecyclerLevel();
+        switch(recyclerLvl)
+        { 
+            case 1:
+                recycle1.setVisible(true);
+                break;
+            case 2:
+                recycle2.setVisible(true);
+                break;
+            case 3:
+                recycle3.setVisible(true);
+                break;
+        }
         makeFadeOut();
     }    
     
@@ -33,7 +51,7 @@ public class GameOverController implements Initializable {
     private void makeFadeOut() { //method for lame transition
         FadeTransition fadeTransition = new FadeTransition();
         fadeTransition.setDuration(Duration.millis(5000));
-        fadeTransition.setNode(root);
+        fadeTransition.setNode(AnchorPane);
         fadeTransition.setFromValue(0);
         fadeTransition.setToValue(1);
         fadeTransition.play();
